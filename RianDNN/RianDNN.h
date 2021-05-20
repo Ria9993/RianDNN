a21fd1068953ff16fa0f89c323c859445cef7e33
Ria9993 <hyunjun2372@gmail.com>
2021-05-20 19:25:59 +0900

    commit

--- RianDNN/RianDNN.h
+++ RianDNN/RianDNN.h
@@ -23,46 +23,96 @@ namespace RianDNN {
 	public:
 		int last_node_num_;
 		int node_num_;
-		string activation_;
-		//None
-		//ReLU
-		vector<vector<double>> weight_;
-		//[nowLayer_node][lastLayer_node]
+		string activation_; //"None" or "ReLU"
+		vector<vector<double>> weight_; //[nowLayer_node][lastLayer_node]
+		vector<vector<double>> weight_grad_; //local gradient
+		vector<double> grad_; //global gradient
 		vector<double> bias_;
 		vector<double> result_;
-		vector<double> local_gradient_;
 		~Layer() {
 		}
 	};
 
 	class DNN {
 	public:
+		//Hyperparam
 		int input_num_;
+		double learning_rate;
+
+		int output_num_;
 		int layer_num_;
 		vector<Layer> layer_;
-		double learning_rate;
+		
+		int forward_step_; //for calc avg(local_decent)
+		double loss_sum_;
+		double loss_;
 		DNN() {
 			layer_num_ = 0;
+			forward_step_ = 0;
+			loss_sum_ = 0;
 		}
 		~DNN() {
 		}
 		void AddLayer(int node_num, string activation);
 		double* Forward(double* input);
-		void Optimize(double loss);
-		double GetAct(string activation, double x);
+		void Optimize(double* target);
+		inline double GetAct(string activation, int layer_num, double x); //activation function
+		inline double GetActDif(string activation, int layer_num, double x); //differential
+		//inline double GetSoftmaxDif(); //Softmax must on outputlayer
+		double GetLoss(double* target); //default = MSE(Mean Squared Error)
+		//inline double GetLossDif();
+		void GradZero(); //reset gradient
 	};
 
-	double DNN::GetAct(string activation, double x) {
+	inline double DNN::GetAct(string activation, int layer_num, double x) {
 		if (activation == "ReLU") {
 			return std::fmax(0, x);
+		}
+		else if (activation == "Softmax") { //Softmax is must with CEE loss
+			double max = 0;
+			for (int i = 0; i < layer_[layer_num].node_num_; i++) {
+				if (i == 0)
+					max = layer_[layer_num].result_[i];
+				else if (max < layer_[layer_num].result_[i]) {
+					max = layer_[layer_num].result_[i];
+				}
+			}
+			double sum = 0;
+			for (int i = 0; i < layer_[layer_num].node_num_; i++) {
+				sum += expf(layer_[layer_num].result_[i] - max);
+			}
+			return expf(x) / sum;
 		}
 		else { //"None"
 			return x;
 		}
 		return 0;
 	}
+	inline double DNN::GetActDif(string activation, int layer_num, double x) {
+		if (activation == "ReLU") {
+			return x > 0 ? 1 : 0;
+		}
+		else if (activation == "Softmax") {
+			//x는 이미 계산된 Softmax(z)
+			return (x * (1 - x));
+		}
+		else { //"None"
+			return 1;
+		}
+		return 0;
+	}
+	double DNN::GetLoss(double * target) {
+		double loss = 0;
+		for (int i = 0; i < layer_[layer_num_ - 1].node_num_; i++) {
+			loss += powf((target[i] - layer_[layer_num_ - 1].result_[i]), 2);
+		}
+		loss_ = loss / layer_[layer_num_ - 1].node_num_;
+		loss_sum_ += loss_;
+		return loss_sum_ / forward_step_;
+	}
 	void DNN::AddLayer(int node_num, string activation) {
 		layer_num_++;
+		output_num_ = node_num;
 		layer_.resize(layer_num_);
 
 		Layer* new_layer = &layer_[layer_num_ - 1];
@@ -73,22 +123,29 @@ namespace RianDNN {
 			new_layer->last_node_num_ = input_num_;
 		else
 			new_layer->last_node_num_ = layer_[layer_num_ - 2].node_num_;
-		new_layer->weight_.resize(new_layer->last_node_num_);
-		//weight xavier initialization
+
+		/* weight HE initialization (sqrt(2) * sqrt(2/(in+out))) */
 		random_device rd;
 		mt19937 rnd(rd());
-		normal_distribution <double> xavier_init(0,(double)2 / (new_layer->last_node_num_ + new_layer->node_num_));
+		normal_distribution <double> HE_init(0, sqrtf(2) * sqrtf((float)2 / (new_layer->last_node_num_ + new_layer->node_num_)));
 		new_layer->weight_.resize(new_layer->node_num_);
+		new_layer->weight_grad_.resize(new_layer->node_num_);
 		for (int i = 0; i < new_layer->node_num_; i++) {
 			new_layer->weight_[i].resize(new_layer->last_node_num_);
-			for (int j = 0; j < new_layer->last_node_num_; j++)
-				new_layer->weight_[i][j] = xavier_init(rnd);
+			new_layer->weight_grad_[i].resize(new_layer->last_node_num_);
+			for (int j = 0; j < new_layer->last_node_num_; j++) {
+				new_layer->weight_[i][j] = HE_init(rnd);
+				new_layer->weight_grad_[i][j] = 0.0f;
+			}
 		}
+		new_layer->grad_.resize(new_layer->node_num_);
 		new_layer->bias_.resize(new_layer->node_num_);
-		for (int i = 0; i < new_layer->node_num_; i++)
-			new_layer->bias_[i] = 0.0f;
 		new_layer->result_.resize(new_layer->node_num_);
-		new_layer->local_gradient_.resize(new_layer->node_num_);
+		for (int i = 0; i < new_layer->node_num_; i++) {
+			new_layer->bias_[i] = 0.0f;
+			new_layer->result_[i] = 0.0f;
+			new_layer->grad_[i] = 0.0f;
+		}
 		return;
 	}
 	double* DNN::Forward(double* input) {
@@ -97,17 +154,68 @@ namespace RianDNN {
 			for (int i = 0; i < now->node_num_; i++) {
 				now->result_[i] = now->bias_[i];
 				for (int j = 0; j < now->last_node_num_; j++) {
-					if (n == 0) //input
+					if (n == 0) {//input
 						now->result_[i] += now->weight_[i][j] * input[j];
-					else 
-						now->result_[i] += now->weight_[i][j] * layer_[n-1].result_[j];
+						now->weight_grad_[i][j] += input[j];
+					}
+					else {
+						now->result_[i] += now->weight_[i][j] * layer_[n - 1].result_[j];
+						now->weight_grad_[i][j] += layer_[n - 1].result_[j];
+					}
 				}
-				now->result_[i] = GetAct(now->activation_, now->result_[i]);
+				now->result_[i] = GetAct(now->activation_, n, now->result_[i]);
+				//now->grad_[i] += GetActDif(now->activation_, n, now->result_[i]);
 			}
 		}
-		return &layer_[layer_num_-1].result_[0];
+		forward_step_++;
+		return &layer_[layer_num_ - 1].result_[0];
 	}
-	void DNN::Optimize(double loss) {
+	void DNN::Optimize(double *target) {
+		GetLoss(target);
+		for (int n = layer_num_ - 1; n >= 0; n--) {
+			Layer* now = &layer_[n];
+			/*Backpropagation*/
+			for (int i = 0; i < now->node_num_; i++) {
+				//Get global gradient
+				if (n == layer_num_ - 1) { //output
+					//MSE loss derivation
+					now->grad_[i] = (2 * fabs(target[i] - now->result_[i])) / now->node_num_;
+					//now->grad_[i] = 2/now->node_num_;
+				}
+				else {
+					for (int j = 0; j < layer_[n + 1].node_num_; j++) {
+						now->grad_[i] += layer_[n + 1].weight_grad_[j][i];
+					}
+				}
 
+				//global gradient * activation gradient
+				now->grad_[i] *= GetActDif(now->activation_, n, now->result_[i]);
+
+				for (int j = 0; j < now->last_node_num_; j++) {
+					//Get Avg weight gradient
+					now->weight_grad_[i][j] /= forward_step_;
+					//local_gradient * global gradient
+					now->weight_grad_[i][j] *= now->grad_[i];
+					//Weight & Bias Update
+					now->weight_[i][j] = now->weight_[i][j] - learning_rate * now->weight_grad_[i][j];
+					now->bias_[i] = now->bias_[i] - learning_rate * now->grad_[i];
+				}
+			}
+		}
+		//GradZero();
+	}
+	void DNN::GradZero() {
+		forward_step_ = 0;
+		loss_sum_ = 0;
+		for (int n = 0; n < layer_num_; n++) {
+			Layer* now = &layer_[n];
+			for (int i = 0; i < layer_[n].node_num_; i++) {
+				now->grad_[i] = 0.0f;
+				for (int j = 0; j < layer_[n].last_node_num_; j++) {
+					now->weight_grad_[i][j] = 0.0f;
+				}
+			}
+		}
+		return;
 	}
 }
--- test.cpp
+++ test.cpp
@@ -9,21 +9,37 @@ int main() {
 	RianDNN::DNN dnn;
 	dnn.learning_rate = 0.01f;
 	//dnn.momentum_rate = 0.1;
-	dnn.input_num_ = 100;
-	dnn.AddLayer(300, "ReLU");
-	dnn.AddLayer(300, "ReLU");
-	dnn.AddLayer(300, "ReLU");
-	dnn.AddLayer(1, "None"); //output layer
-	
+	dnn.input_num_ = 2;
+	//dnn.AddLayer(300, "ReLU");
+	//dnn.AddLayer(300, "ReLU");
+	//dnn.AddLayer(300, "ReLU");
+	dnn.AddLayer(2, "None"); //output layer
+	//dnn.AddLayer(2, "Softmax"); 
+
+	/*DEBUG*/
 	random_device rd;
 	mt19937 rnd(rd());
 	uniform_int_distribution <> rd100(0, 100);
 	for (int i = 0; i < 1000; i++) {
 		double input[110];
-		for (int j = 0; j < 100; j++)
-			input[j] = rd100(rnd);
+		for (int j = 0; j < 100; j++) {
+			//input[j] = rd100(rnd);
+			input[j] = 0.1f;
+		}
 		double* output = dnn.Forward(input);
-		printf("%4d : %lf\n", i,*output);
+		double target[10] = { 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f };
+		dnn.Optimize(target);
+		printf("weights and bias\n");
+		for (int j = 0; j < dnn.layer_[dnn.layer_num_ - 1].node_num_; j++) {
+			for (int k = 0; k < dnn.layer_[dnn.layer_num_ - 1].last_node_num_; k++) {
+				printf("%8.5lf(%8.5lf) ", dnn.layer_[dnn.layer_num_ - 1].weight_[j][k], dnn.layer_[dnn.layer_num_ - 1].weight_grad_[j][k]);
+			}
+			printf(" Bias : %8.5lf  Grad : %8.5lf\n", dnn.layer_[dnn.layer_num_ - 1].bias_[j],dnn.layer_[dnn.layer_num_-1].grad_[j]);
+		}
+		printf("outputs\n");
+		printf("%7.4lf  %7.4lf   loss : %8.5lf\n", *output, output[1], dnn.loss_);
+		dnn.GradZero();
+		scanf("%*c"); //pause
 	}
 	return 0;
 }
